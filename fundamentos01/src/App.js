7b3a604f4d8f130248cb8529ea9a94c8af2512b2
Guilherme Barbosa Lopes <guiblopes4@gmail.com>
2021-05-23 20:34:11 +0000

    Aula 01 - Primeiro Componente

---
+++ fundamentos01/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import OlaMundo from './Components/Básico/OlaMundo';
+
+class App extends Component {
+  render() {
+   return (
+     <div>
+       <h1>Aula 1 - Fundamentos</h1>
+
+       <OlaMundo />
+     </div>
+   );
+   }
+}
+
+export default App;
